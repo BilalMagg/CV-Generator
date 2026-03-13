35e23eea06a1475cb4ce48a404fef72757bf0d94
Bilal <gygcjg55@gmail.com>
2026-03-13 23:22:26 +0000

    Add swagger

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,23 +1,56 @@
+using backend.src.features.user.entity;
+using backend.src.features.auth.entity;
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// ------------------------
+// 1. Configure services
+// ------------------------
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Add controllers
+builder.Services.AddControllers();
+
+// Add Swagger for API testing
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure EF Core with PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// If you want JWT auth later, you can configure it here
+// builder.Services.AddAuthentication(...);
+
+// Register your services / repositories here
+// builder.Services.AddScoped<IUserService, UserService>();
+// builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// ------------------------
+// 2. Configure middleware
+// ------------------------
+
+// Enable Swagger
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+// Enable HTTPS
 app.UseHttpsRedirection();
 
+// Enable authentication & authorization if used
+// app.UseAuthentication();
+// app.UseAuthorization();
 
+// Map controllers
+app.MapControllers();
+
+// ------------------------
+// 3. Run the app
+// ------------------------
 app.Run();
-
