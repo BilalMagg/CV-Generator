c895abc046e3b99cb31cdcc4766c82eda6fe2b31
Bilal <gygcjg55@gmail.com>
2026-04-24 20:05:14 +0100

    Add initial script for VMs

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,80 +2,47 @@ terraform {
   required_providers {
     virtualbox = {
       source  = "terra-farm/virtualbox"
-      version = ">=0.3.1"
+      version = "0.2.2-alpha.1"
     }
   }
 }
 
-provider "virtualbox" {}
-
-# Create the VM
-resource "virtualbox_vm" "app_vm" {
-  name   = "DockerHostVM"
-  image  = "ubuntu_24_04"  # Name of the Ubuntu box installed in VirtualBox
+resource "virtualbox_vm" "terra_server" {
+  name   = "terra-server"
+  # Use a working Ubuntu 22.04 box
+  image  = "https://vagrantcloud.com/generic/boxes/ubuntu2204/versions/4.3.12/providers/virtualbox.box"
   cpus   = 2
-  memory = 2048
+  memory = "2048 mib"
+  user_data = file("${path.module}/scripts/user_data.sh")
 
-  # Port forwarding for SSH, HTTP, Postgres
-  network_adapter {
-    type           = "hostonly"
-    host_interface = "vboxnet0"  # default VirtualBox host-only adapter
-  }
-
+  # Adapter 1: NAT (for internet access)
   network_adapter {
     type = "nat"
-    nat_network {
-      name = "NatNetwork"
-    }
-
-    # Forward ports from host to guest
-    nat_network_port_forward {
-      host_port      = 2222
-      guest_port     = 22
-      protocol       = "tcp"
-      description    = "SSH"
-    }
-
-    nat_network_port_forward {
-      host_port      = 5000
-      guest_port     = 5000
-      protocol       = "tcp"
-      description    = "ASP.NET"
-    }
-
-    nat_network_port_forward {
-      host_port      = 8000
-      guest_port     = 8000
-      protocol       = "tcp"
-      description    = "FastAPI"
-    }
-
-    nat_network_port_forward {
-      host_port      = 5432
-      guest_port     = 5432
-      protocol       = "tcp"
-      description    = "Postgres"
-    }
   }
 
-  # Boot script to install Docker and run containers
-  provisioner "file" {
-    source      = "setup.sh"
-    destination = "/home/ubuntu/setup.sh"
+  # Adapter 2: Host-Only (for direct access)
+  network_adapter {
+    type           = "hostonly"
+    host_interface = "VirtualBox Host-Only Ethernet Adapter"
   }
+}
 
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /home/ubuntu/setup.sh",
-      "sudo /home/ubuntu/setup.sh"
-    ]
+output "vm_ip_hostonly" {
+  value = virtualbox_vm.terra_server.network_adapter[1].ipv4_address
+  description = "VM IP on host-only network"
+}
 
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
-      host        = "127.0.0.1"
-      port        = 2222
-    }
+output "ssh_command" {
+  value = "ssh vagrant@${virtualbox_vm.terra_server.network_adapter[1].ipv4_address}"
+}
+
+output "service_urls" {
+  value = {
+    ssh       = "ssh vagrant@${virtualbox_vm.terra_server.network_adapter[1].ipv4_address}"
+    frontend  = "http://${virtualbox_vm.terra_server.network_adapter[1].ipv4_address}:4200"
+    backend   = "http://${virtualbox_vm.terra_server.network_adapter[1].ipv4_address}:5000"
+    ai_agents = "http://${virtualbox_vm.terra_server.network_adapter[1].ipv4_address}:8000"
+    keycloak  = "http://${virtualbox_vm.terra_server.network_adapter[1].ipv4_address}:8080"
+    postgres  = "${virtualbox_vm.terra_server.network_adapter[1].ipv4_address}:5432"
   }
 }
