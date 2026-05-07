96487fed53343f3c0a2cc13d93487a39ee29ffea
Bilal <gygcjg55@gmail.com>
2026-05-07 12:39:25 +0100

    Adding the best job extractor agent in the history

--- ai_agents/services/common-tools/cvtools/core/tools/__init__.py
+++ ai_agents/services/common-tools/cvtools/core/tools/__init__.py
@@ -1,10 +1,6 @@
-# Tools package
-from app.core.tools.pdf_converter import html_to_pdf, latex_to_pdf
-from app.core.tools.minio_storage import (
-    get_minio_client,
-    upload_pdf,
-    download_pdf,
-)
+from .pdf_converter import html_to_pdf, latex_to_pdf
+from .minio_storage import get_minio_client, upload_pdf, download_pdf
+from .pdf_extractor import extract_text_from_pdf, extract_text_from_pdf_bytes
 
 __all__ = [
     "html_to_pdf",
@@ -12,4 +8,6 @@ __all__ = [
     "get_minio_client",
     "upload_pdf",
     "download_pdf",
+    "extract_text_from_pdf",
+    "extract_text_from_pdf_bytes",
 ]
