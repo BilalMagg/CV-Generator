79aa71cfaa1bdeb40718b2aa595dd8871dab3bf0
Bilal <gygcjg55@gmail.com>
2026-05-15 15:45:14 +0100

    Fix: frontend environment urls

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/user-content',
+  apiUrl: '',
   gatewayUrl: '',
   useTempAuth: false,
   tempUserId: '00000000-0000-0000-0000-000000000001',
